e7101d832ca8c6c14c0980cb72b282f685d586c8
yuyoshi2001 <s20c3048wa@s.chibakoudai.jp>
2023-05-25 15:27:07 +0900

    船の無敵点滅追加

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -7,16 +7,21 @@ public class ShipController : MonoBehaviour
     public float maxSpeed = 20f; // 船の速度上限
     public float stunDuration = 2f; // スタン状態の期間
     public float invincibleDuration = 5f; // 無敵時間の期間
+    public float blinkInterval = 0.2f; // 点滅の間隔
 
     private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
     private bool isStunned = false; // 船がスタン状態かどうか
     private float stunTimer = 0f; // スタン状態の経過時間
     private bool isInvincible = false; // 船が無敵状態かどうか
     private float invincibleTimer = 0f; // 無敵状態の経過時間
+    private bool isBlinking = false; // 点滅中かどうか
+    private float blinkTimer = 0f; // 点滅の経過時間
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
@@ -49,6 +54,8 @@ public class ShipController : MonoBehaviour
                 isInvincible = true;
                 stunTimer = 0f;
                 invincibleTimer = 0f;
+                isBlinking = true;
+                blinkTimer = 0f;
             }
         }
 
@@ -62,6 +69,21 @@ public class ShipController : MonoBehaviour
             {
                 isInvincible = false;
                 invincibleTimer = 0f;
+                isBlinking = false;
+                spriteRenderer.enabled = true; // 点滅を停止し、表示を元に戻す
+            }
+        }
+
+        if (isBlinking)
+        {
+            // 点滅の経過時間を更新
+            blinkTimer += Time.fixedDeltaTime;
+
+            // 点滅の間隔を超えた場合、表示を切り替える
+            if (blinkTimer >= blinkInterval)
+            {
+                spriteRenderer.enabled = !spriteRenderer.enabled;
+                blinkTimer = 0f;
             }
         }
     }
@@ -73,6 +95,9 @@ public class ShipController : MonoBehaviour
         {
             isStunned = true; // スタン状態にする
             stunTimer = 0f; // スタン状態の経過時間をリセット
+            spriteRenderer.enabled = false; // 表示を切り替えて点滅を開始
+            isBlinking = true;
+            blinkTimer = 0f;
         }
     }
 }
