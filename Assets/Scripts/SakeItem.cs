f48d104dbf7e1b777512426c183f9fcea158b997
Purin9722 <s20c3071ke@s.chibakoudai.jp>
2023-05-30 15:37:36 +0900

    UI

--- Assets/Scripts/SakeItem.cs
+++ Assets/Scripts/SakeItem.cs
@@ -2,10 +2,17 @@ using UnityEngine;
 
 public class SakeItem : Item
 {
+    public Sprite sprite;
+
     public float slowdownDuration = 5f; // 酒の効果が続く時間
     public float slowdownFactor = 0.5f; // 酒の効果による速度減少率
 
     private ShipController player; // 酒の効果を受けるプレイヤー
+
+     public override Sprite GetItemImage()
+    {
+        return sprite;
+    }
 
     void Start() {
         player = GameObject.Find("Ship").GetComponent<ShipController>();
